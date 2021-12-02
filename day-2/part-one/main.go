4ae8fe7d22f5e0da977fff381155fa62bb8f5b44
SeNicko <sebastianflajszer@gmail.com>
2021-12-02 16:11:20 +0100

    Solve the fist part of the second day challenge

---
+++ day-2/part-one/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./in.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	horizontal := 0
+	depth := 0
+
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), " ")
+
+		direction := row[0]
+
+		value, err := strconv.Atoi(row[1])
+		if err != nil {
+			panic(err)
+		}
+
+		switch direction {
+		case "forward":
+			horizontal += value
+		case "up":
+			depth -= value
+		case "down":
+			depth += value
+		}
+	}
+
+	fmt.Println(horizontal * depth)
+}
